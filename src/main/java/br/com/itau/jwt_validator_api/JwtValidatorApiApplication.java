19dd83d86445062ff0289845f35a7ab614a94bc6
Leonardo Stanchak <leonardostanchak@hotmail.com>
2025-07-24 15:29:41 -0300

    feature: Arquitetura projeto base

---
+++ src/main/java/br/com/itau/jwt_validator_api/JwtValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.jwt_validator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/jwt_validator_api/JwtValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.jwt_validator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
